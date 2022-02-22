2e970bd6ad5c231a1f9d6c809e07d76ca54b6b0d
Martin <martin.trojan.372@gmail.com>
2022-02-22 21:34:55 +0100

    feat: initialized project with c-lion

---
+++ code/src/State_machine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marti on 2/22/2022.
+//
+
+#include "State_machine.h"
---
+++ code/src/State_machine.h
@@ -0,0 +1,16 @@
+//
+// Created by marti on 2/22/2022.
+//
+
+#ifndef UNTITLED_STATE_MACHINE_H
+#define UNTITLED_STATE_MACHINE_H
+
+
+template<typename StateT, typename TriggerT> class State_machine {
+private:
+    StateT stateT;
+    TriggerT triggerT;
+};
+
+
+#endif //UNTITLED_STATE_MACHINE_H
