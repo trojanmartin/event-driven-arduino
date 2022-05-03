3d8a4878dc967b5b6dc207e35b2c92cb9b6a33c2
Martin <martin.trojan.372@gmail.com>
2022-05-03 17:49:41 +0200

    feat: blinking example

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,16 +1,10 @@
 #include <Interro.hpp>
 
-enum State
+enum States
 {
-    Idle,
-    Quarter,
-    Half,
-    ThreQuarter
-};
-enum SecondMachineStates
-{
-    SecondIdle = 4,
-    Active
+    LedOn,
+    LedOff,
+    Idle
 };
 
 enum Events
@@ -18,114 +12,54 @@ enum Events
     ButtonClicked,
     TimeElapsed,
 };
-enum SecondMachineEvents
-{
-    PinChanged = 3
-};
 
+Timer1Trigger timerTrigger;
 ButtonTrigger buttonTrigger;
-Timer1Trigger timer1;
-Timer3Trigger timer3;
-PinChangeTrigger0 pinChange0Trigger;
 
-StateMachine machine(Idle);
-static const int8_t idle_state_table[]{
-    ButtonClicked, Quarter,
+StateMachine blinkingMachine(States::Idle);
+
+static const int8_t idleStateTable[]{
+    Events::ButtonClicked, States::LedOn,
     -1};
 
-static const int8_t quarter_state_table[]{
-    ButtonClicked, Idle,
-    TimeElapsed, Half,
+static const int8_t ledOnStateTable[]{
+    Events::ButtonClicked, States::Idle,
+    Events::TimeElapsed, States::LedOff,
     -1};
 
-static const int8_t half_table[]{
-    ButtonClicked, Idle,
-    TimeElapsed, ThreQuarter,
+static const int8_t ledOffStateTable[]{
+    Events::ButtonClicked, States::Idle,
+    Events::TimeElapsed, States::LedOn,
     -1};
 
-static const int8_t threQuarter_table[]{
-    ButtonClicked, Idle,
-    TimeElapsed, Quarter,
-    -1};
-
-StateMachine secondMachine(SecondMachineStates::SecondIdle);
-static const int8_t second_idle_state_table[]{
-    PinChanged, Active,
-    -1};
-
-static const int8_t second_active_state_table[]{
-    PinChanged, SecondIdle,
-    -1};
-
-void setLedLuminance(double value)
+void TurnOnLed()
 {
-    auto pwmConfigurator = timer1.getPwmConfigurator();
-    pwmConfigurator.setUpOutput(TimerPwmOutput::C, value);
 }
 
-uint8_t pinAa = 52;
-
-void configureBlinkingMachine()
+void TurnOffLed()
 {
-    machine.configure(Idle)
-        .onEvent(idle_state_table)
-        .onEntry([](int8_t event)
-                 { setLedLuminance(0); })
-
-        .onExit([](int8_t event)
-                { digitalWrite(pinAa, !digitalRead(pinAa)); });
-
-    machine.configure(Quarter)
-        .onEvent(quarter_state_table)
-        .onEntry([](int8_t event)
-                 { setLedLuminance(0.25); });
-
-    machine.configure(Half)
-        .onEvent(half_table)
-        .onEntry([](int8_t event)
-                 { setLedLuminance(0.75); });
-
-    machine.configure(ThreQuarter)
-        .onEvent(threQuarter_table)
-        .onEntry([](int8_t event)
-                 { setLedLuminance(1); });
-
-    timer3.configure(TimerMode::CTC)
-        .onTimeElapsed(1000, TimeElapsed);
-
-    buttonTrigger.configure(5)
-        .onClick(ButtonClicked);
-
-    auto pwmConfigurator = timer1.configure(TimerMode::PWM)
-                               .setPrescalerValue(Prescalers::Prescaler64)
-                               .setPwmMode(PwmMode::FastPwm8Bit);
-
-    pwmConfigurator.setUpOutput(TimerPwmOutput::C, 0);
-}
-
-void configureSecondMachine()
-{
-    secondMachine.configure(SecondIdle)
-        .onEvent(second_idle_state_table)
-        .onEntry([](int8_t event)
-                 { Serial.println("Second machine idle entry"); });
-
-    secondMachine.configure(Active)
-        .onEvent(second_active_state_table)
-        .onEntry([](int8_t event)
-                 { Serial.println("Second machine Active entry"); });
-
-    pinChange0Trigger.configure()
-        .enableDigitalPin(pinAa)
-        .onOccurrence(PinChanged);
 }
 
 void setup()
 {
-    Serial.begin(9600);
+    blinkingMachine.configure(States::Idle)
+        .onEvent(idleStateTable);
 
-    configureBlinkingMachine();
-    configureSecondMachine();
+    blinkingMachine.configure(States::LedOn)
+        .onEvent(ledOnStateTable)
+        .onEntry([](int8_t state)
+                 { TurnOnLed(); });
+
+    blinkingMachine.configure(States::LedOff)
+        .onEvent(ledOffStateTable)
+        .onEntry([](int8_t state)
+                 { TurnOffLed(); });
+
+    timerTrigger.configure(TimerMode::CTC)
+        .onTimeElapsed(1000, Events::TimeElapsed);
+
+    buttonTrigger.configure(5)
+        .onClick(Events::ButtonClicked);
 }
 
 void loop()
